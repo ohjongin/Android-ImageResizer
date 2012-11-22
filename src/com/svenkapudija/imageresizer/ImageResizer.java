f1a0cad4c892359f97dcb68e8eb2d1c15a45998d
Sven Kapudija <sven.kapudija@gmail.com>
2012-11-22 22:39:53 +0100

    Added support for different dimension units

--- src/com/svenkapudija/imageresizer/ImageResizer.java
+++ src/com/svenkapudija/imageresizer/ImageResizer.java
@@ -1,22 +1,112 @@
 package com.svenkapudija.imageresizer;
 
-import java.io.File;
-
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Log;
 
-public interface ImageResizer {
+public class ImageResizer {
 
-	public Bitmap resize(Bitmap original, int size);
-	public Bitmap resize(Bitmap original, int size, ResizeMode mode);
+	private static final String TAG = ImageResizer.class.getName();
 	
-	public Bitmap resize(Bitmap original, int width, int height);
-	public Bitmap resize(Bitmap original, int width, int height, ResizeMode mode);
+	/*** RESIZE ***/
+	public static Bitmap resize(Bitmap original, int size) {
+		return null;
+	}
 	
-	public Bitmap crop(Bitmap original, int width, int height);
-	public Bitmap crop(Bitmap original, int x, int y, int width, int height);
+	public static Bitmap resize(Bitmap original, int size, ResizeMode mode) {
+		return null;
+	}
 	
-	public Bitmap rotate(Bitmap original, ImageRotation rotation);
+	public static Bitmap resize(Bitmap original, int size, DimensionUnit unit, Context ... context) {
+		try {
+			size = DimensionUnit.convertToPixels(unit, size, context);
+		} catch (ImageResizerException e) {
+			Log.e(TAG, e.getMessage());
+			return null;
+		}
+		
+		return null;
+	}
 	
-	public void save(File saveLocation);
+	public static Bitmap resize(Bitmap original, int size, ResizeMode mode, DimensionUnit unit, Context ... context) {
+		try {
+			size = DimensionUnit.convertToPixels(unit, size, context);
+		} catch (ImageResizerException e) {
+			Log.e(TAG, e.getMessage());
+			return null;
+		}
+		
+		return null;
+	}
+	
+	public static Bitmap resize(Bitmap original, int width, int height) {
+		return null;
+	}
+	
+	public static Bitmap resize(Bitmap original, int width, int height, ResizeMode mode) {
+		return null;
+	}
+	
+	public static Bitmap resize(Bitmap original, int width, int height, DimensionUnit unit, Context ... context) {
+		try {
+			width = DimensionUnit.convertToPixels(unit, width, context);
+			height = DimensionUnit.convertToPixels(unit, height, context);
+		} catch (ImageResizerException e) {
+			Log.e(TAG, e.getMessage());
+			return null;
+		}
+		
+		return null;
+	}
+	
+	public static Bitmap resize(Bitmap original, int width, int height, ResizeMode mode, DimensionUnit unit, Context ... context) {
+		try {
+			width = DimensionUnit.convertToPixels(unit, width, context);
+			height = DimensionUnit.convertToPixels(unit, height, context);
+		} catch (ImageResizerException e) {
+			Log.e(TAG, e.getMessage());
+			return null;
+		}
+		
+		return null;
+	}
+	
+	/*** CROP ***/
+	public static Bitmap crop(Bitmap original, int width, int height) {
+		return null;
+	}
+	
+	public static Bitmap crop(Bitmap original, int x, int y, int width, int height) {
+		return null;
+	}
+	
+	public static Bitmap crop(Bitmap original, int width, int height, DimensionUnit unit, Context ... context) {
+		try {
+			width = DimensionUnit.convertToPixels(unit, width, context);
+			height = DimensionUnit.convertToPixels(unit, height, context);
+		} catch (ImageResizerException e) {
+			Log.e(TAG, e.getMessage());
+			return null;
+		}
+		
+		return null;
+	}
+	
+	public static Bitmap crop(Bitmap original, int x, int y, int width, int height, DimensionUnit unit, Context ... context) {
+		try {
+			width = DimensionUnit.convertToPixels(unit, width, context);
+			height = DimensionUnit.convertToPixels(unit, height, context);
+		} catch (ImageResizerException e) {
+			Log.e(TAG, e.getMessage());
+			return null;
+		}
+		
+		return null;
+	}
+	
+	/*** ROTATE ***/
+	public static Bitmap rotate(Bitmap original, ImageRotation rotation) {
+		return null;
+	}
 	
 }
--- src/com/svenkapudija/imageresizer/demo/MainActivity.java
+++ src/com/svenkapudija/imageresizer/demo/MainActivity.java
@@ -1,9 +1,14 @@
 package com.svenkapudija.imageresizer.demo;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import com.svenkapudija.imageresizer.DimensionUnit;
+import com.svenkapudija.imageresizer.ImageResizer;
+import com.svenkapudija.imageresizer.ImageRotation;
 import com.svenkapudija.imageresizer.R;
+import com.svenkapudija.imageresizer.ResizeMode;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,30 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Bitmap myImage = null;
+        
+        // The simplest case - width and height are the same size
+        ImageResizer.resize(myImage, 500);
+        
+        // Different width and height
+        ImageResizer.resize(myImage, 640, 480);
+        
+        // Choose ResizeMode - FIT_TO_WIDTH, FIT_TO_HEIGHT, FIT_EXACT or AUTOMATIC
+        ImageResizer.resize(myImage, 640, 480, ResizeMode.FIT_TO_WIDTH);
+        
+        // Resize according to DP's (library will convert those into pixels
+        // based on device screen density)
+        ImageResizer.resize(myImage, 200, 200, DimensionUnit.DP, this);
+        
+        // Basic croping
+        ImageResizer.crop(myImage, 500, 500);
+
+        // Croping with specified starting X and Y point
+        ImageResizer.crop(myImage, 50, 50, 640, 480);
+        
+        // Rotate an image for every 90 degrees
+        ImageResizer.rotate(myImage, ImageRotation.CW_90);
         
     }
 }
